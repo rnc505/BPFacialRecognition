59baf09c9ef10a27cc11ff3d00612b613ea4c0b1
Robby Cohen <robert.n.cohen@vanderbilt.edu>
2014-01-22 23:13:18 -0600

    Changed project from an Application to a static library

---
+++ BPFacialRecognition/BPFacialRecognition/BPFacialRecognition.h
@@ -0,0 +1,13 @@
+//
+//  BPFacialRecognition.h
+//  BPFacialRecognition
+//
+//  Created by Robby Cohen on 1/22/14.
+//  Copyright (c) 2014 BP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BPFacialRecognition : NSObject
+
+@end
---
+++ BPFacialRecognition/BPFacialRecognition/BPFacialRecognition.m
@@ -0,0 +1,13 @@
+//
+//  BPFacialRecognition.m
+//  BPFacialRecognition
+//
+//  Created by Robby Cohen on 1/22/14.
+//  Copyright (c) 2014 BP. All rights reserved.
+//
+
+#import "BPFacialRecognition.h"
+
+@implementation BPFacialRecognition
+
+@end
