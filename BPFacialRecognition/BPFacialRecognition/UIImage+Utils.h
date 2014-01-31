eddc636fcebda34e9a17def72dfcff6567ac014b
Robby Cohen <robert.n.cohen@vanderbilt.edu>
2014-01-31 10:47:57 -0600

    UIImage Utils

---
+++ BPFacialRecognition/BPFacialRecognition/UIImage+Utils.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Utils.h
+//  BPFacialRecognition
+//
+//  Created by Robby Cohen on 1/30/14.
+//  Copyright (c) 2014 BP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Utils)
+
+@end
---
+++ BPFacialRecognition/BPFacialRecognition/UIImage+Utils.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+Utils.m
+//  BPFacialRecognition
+//
+//  Created by Robby Cohen on 1/30/14.
+//  Copyright (c) 2014 BP. All rights reserved.
+//
+
+#import "UIImage+Utils.h"
+
+@implementation UIImage (Utils)
+
+@end
