954123232f160e691129a57c2003c55e79cc16a2
Robby Cohen <robert.n.cohen@vanderbilt.edu>
2014-02-02 15:49:38 -0600

    Got it ready for library

---
+++ BPFacialRecognition/BPFacialRecognition/BPFacialRecognition.h
@@ -0,0 +1,12 @@
+//
+//  BPFacialRecognition.h
+//  BPFacialRecognition
+//
+//  Created by Robby Cohen on 1/22/14.
+//  Copyright (c) 2014 BP. All rights reserved.
+//
+
+#import "BPFacialRecognizer.h"
+#import "BPRecognitionResult.h"
+#import "BPPerson.h"
+#import "BPUtil.h"
